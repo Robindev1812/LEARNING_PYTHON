4505b23cae25524f565abe8e5d8aa25905af9622
Robinson Aparicio <apariciodev1812@gmail.com>
2024-10-18 11:05:09 -0500

    Add variables

---
+++ 01_variables.py
@@ -0,0 +1,25 @@
+# En python, basta con escribir el nombre de la variable y asignarle su valor.
+name = "Robinson"
+
+# Tipos de datos primitivos.
+## Entero (Integer)
+number_integer = 10
+print(f"El numero {number_integer} es un tipo de dato {type(number_integer)} ")
+
+## Flotante (Float)
+number_float = 3.15
+print(f"El numero {number_float} es un tipo de dato {type(number_float)} ")
+
+## Complejo
+number_complex = 2 + 5j
+print(f"El numero {number_complex} es un tipo de dato {type(number_complex)} ")
+
+## Bool
+type_bool = True # or False
+print(f"El tipo de {type_bool} es un tipo de dato {type(type_bool)} ")
+
+## Cadena (String)
+type_string = "Cadena"
+print(f"El tipo de {type_string} es un tipo de dato {type(type_string)} ")
+
+
